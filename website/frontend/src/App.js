c06e7df1a3efd611144afbade3d8edcec39018e1
Kanishkamohan2411 <kanishkamnkl@gmail.com>
2024-09-18 20:26:22 +0530

    jh

--- website/frontend/src/App.js
+++ website/frontend/src/App.js
@@ -4,8 +4,10 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import {Navbar, Footer} from './Components'
 import {Events,Events_secA,Events_secB,Events_secC,Events_secD,Events_secE,Home,Contact,Accomodation} from './pages'
 
-import './App.css';
-import './index.css';
+
+import "./App.css";
+import"./index.css";
+import { Description, EventTwo_des } from "./pages";
 
 const App = () => {
   const location = useLocation();
@@ -17,14 +19,11 @@ const App = () => {
     <div>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<Events />} />
+        <Route path="/" element={<Description />} />
+        <Route path="/" element={<EventTwo_des />} />
         <Route path="/Events_secA" element={<Events_secA />} />
-        <Route path="/Events_secB" element={<Events_secB />} />
-        <Route path="/Events_secC" element={<Events_secC />} />
-        <Route path="/Events_secD" element={<Events_secD />} />
-        <Route path="/Events_secE" element={<Events_secE />} />
-        <Route path="/contact" element={<Contact />} />
+
+       
         <Route path="/accomodation" element={<Accomodation />} />
       </Routes>
      <Footer/>
