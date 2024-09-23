c1f3fe9ccc4dbbb510f0f6bc8cbd100cadc06f6a
gopi7104 <havocgopi1403@gmail.com>
2024-09-23 16:32:12 +0530

    pay, teams page

--- website/frontend/src/pages/Teams/Teams.jsx
+++ website/frontend/src/pages/Teams/Teams.jsx
@@ -1,30 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Teams.css';
-import {Team_card} from "../../widgets"
+import { Team_card, Modal } from "../../widgets";
 import heading from '../../asset/team.svg';
-import bg from '../../asset/teamBg.webp';
 import boy from '../../asset/Subtract.png';
+import Genral from '../../asset/genral-sec.svg';
+import Joint from '../../asset/joint-sec.svg';
 
 const Teams = () => {
+  const [selectedTeamMember, setSelectedTeamMember] = useState(null); // To store the selected team member's data
+  const [isModalOpen, setIsModalOpen] = useState(false); // Modal open/close state
+
+  // Function to open modal with specific team member data
+  const openModal = (teamData) => {
+    setSelectedTeamMember(teamData); // Store the clicked team member's data
+    setIsModalOpen(true); // Open the modal
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setIsModalOpen(false); // Close the modal
+    setSelectedTeamMember(null); // Reset selected team member
+  };
+
+  const teamMembers = [
+    {
+      image: boy,
+      name: "Solai Dhanush",
+      designation: "General Secretary",
+      designation_img: Genral,
+    },
+    {
+      image: boy,
+      name: "Solai Dhanush",
+      designation: "Joint Secretary",
+      designation_img: Joint,
+    },
+    {
+      image: boy,
+      name: "Solai Dhanush",
+      designation: " Social Media",
+      designation_img: Joint,
+    },
+    {
+      image: boy,
+      name: "Solai Dhanush",
+      designation: "Joint Secretary",
+      designation_img: Joint,
+    },
+    {
+      image: boy,
+      name: "Solai Dhanush",
+      designation: "Joint Secretary",
+      designation_img: Joint,
+    },
+    {
+      image: boy,
+      name: "Solai Dhanush",
+      designation: "Joint Secretary",
+      designation_img: Joint,
+    },
+    // Add more members here...
+  ];
+
   return (
     <div className='Eee__teams-main'>
-        <div className="Eee__teams-heading">
-            <img src={heading} alt="Team" />
-        </div>
+      <div className="Eee__teams-heading">
+        <img className="Eee__teams-hd-img" src={heading} alt="Team" />
+      </div>
 
-            <div className="Eee__teams-hd-holder">
+      <div className="Eee__teams-card-container">
+        {teamMembers.map((teamMember, index) => (
+          <div key={index} onClick={() => openModal(teamMember)}>
             <Team_card
-            image={boy}
-            name="Solai Dhanush"
-            dest="General Secretary"
+              image={teamMember.image}
+              name={teamMember.name}
+              designation={teamMember.designation}
+              designation_img={teamMember.designation_img}
             />
-            </div>
-       
+          </div>
+        ))}
+      </div>
 
-
-      
+      {/* Render Modal only when it's open */}
+      {isModalOpen && selectedTeamMember && (
+        <Modal
+          heading={selectedTeamMember.name}
+          pic={selectedTeamMember.image}
+          content_1={selectedTeamMember.designation} 
+          content_2="" // You can customize this
+          onClose={closeModal} // Close modal on button click
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Teams
+export default Teams;
--- website/frontend/src/widgets/Team_card/Team_card.jsx
+++ website/frontend/src/widgets/Team_card/Team_card.jsx
@@ -2,12 +2,31 @@ import React from 'react'
 import './Team_card.css'
 
 
-const Team_card = ({image,name,dest}) => {
+
+
+
+const Team_card = ({image,name,designation,designation_img}) => {
   return (
     <div>
         <div className="team__card-main">
+          <div className="team__card-holder">
+            <div className="team__card-img-holder">
+            <img className='team__card-img'src={image} alt="Team member" />
+            </div>
 
-                <img src="" alt="" />
+            <div className="tc-tot-img-txt">
+            <div className="team__card-dest-holder">
+              <img className='tm__dest-img'src={designation_img}  alt="Designation img" />
+            </div>
+            <div className="tc-name-dest-holder">
+            <p className='team__card-name'> {name}</p>
+            <p className='team__card-dest'>{designation}</p>
+            </div>
+            
+            </div>
+          </div>
+
+               
 
             </div>
         </div>
