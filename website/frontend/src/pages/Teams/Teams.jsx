369116adbb8d345c89013b5deb6ee7fd98396d5b
gopi7104 <havocgopi1403@gmail.com>
2024-09-28 11:42:12 +0530

    team_card

--- website/frontend/src/pages/Teams/Teams.jsx
+++ website/frontend/src/pages/Teams/Teams.jsx
@@ -1,66 +1,28 @@
 import React, { useState } from 'react';
+import {
+  teamStaffs,
+  teamSecond,
+  teamFourth,
+  teamThird
+} from '../../constants/teams';
+import heading from '../../asset/team.svg';
 import './Teams.css';
 import { Team_card, Modal } from "../../widgets";
-import heading from '../../asset/team.svg';
-import boy from '../../asset/Subtract.png';
-import Genral from '../../asset/genral-sec.svg';
-import Joint from '../../asset/joint-sec.svg';
+
 
 const Teams = () => {
-  const [selectedTeamMember, setSelectedTeamMember] = useState(null); // To store the selected team member's data
-  const [isModalOpen, setIsModalOpen] = useState(false); // Modal open/close state
+  const [selectedTeamMember, setSelectedTeamMember] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // Function to open modal with specific team member data
   const openModal = (teamData) => {
-    setSelectedTeamMember(teamData); // Store the clicked team member's data
-    setIsModalOpen(true); // Open the modal
+    setSelectedTeamMember(teamData);
+    setIsModalOpen(true);
   };
 
-  // Function to close the modal
   const closeModal = () => {
-    setIsModalOpen(false); // Close the modal
-    setSelectedTeamMember(null); // Reset selected team member
+    setIsModalOpen(false);
+    setSelectedTeamMember(null);
   };
-
-  const teamMembers = [
-    {
-      image: boy,
-      name: "Solai Dhanush",
-      designation: "General Secretary",
-      designation_img: Genral,
-    },
-    {
-      image: boy,
-      name: "Solai Dhanush",
-      designation: "Joint Secretary",
-      designation_img: Joint,
-    },
-    {
-      image: boy,
-      name: "Solai Dhanush",
-      designation: " Social Media",
-      designation_img: Joint,
-    },
-    {
-      image: boy,
-      name: "Solai Dhanush",
-      designation: "Joint Secretary",
-      designation_img: Joint,
-    },
-    {
-      image: boy,
-      name: "Solai Dhanush",
-      designation: "Joint Secretary",
-      designation_img: Joint,
-    },
-    {
-      image: boy,
-      name: "Solai Dhanush",
-      designation: "Joint Secretary",
-      designation_img: Joint,
-    },
-    // Add more members here...
-  ];
 
   return (
     <div className='Eee__teams-main'>
@@ -68,27 +30,81 @@ const Teams = () => {
         <img className="Eee__teams-hd-img" src={heading} alt="Team" />
       </div>
 
+      <div className="Eee__sub-heading">
+        <p className='Eee__sub-heading-p'>Staffs Incharge</p>
+      </div>
+
       <div className="Eee__teams-card-container">
-        {teamMembers.map((teamMember, index) => (
-          <div key={index} onClick={() => openModal(teamMember)}>
+        {teamStaffs.map((teamStaffs, index) => (
+          <div key={index} onClick={() => openModal(teamStaffs)}>
             <Team_card
-              image={teamMember.image}
-              name={teamMember.name}
-              designation={teamMember.designation}
-              designation_img={teamMember.designation_img}
+              image={teamStaffs.image}
+              name={teamStaffs.name}
+              designation={teamStaffs.designation}
+              designation_img={teamStaffs.designation_img}
             />
           </div>
         ))}
       </div>
 
-      {/* Render Modal only when it's open */}
+      <div className="Eee__sub-heading">
+        <p className='Eee__sub-heading-p'>Final Year Student</p>
+      </div>
+
+      <div className="Eee__teams-card-container">
+        {teamFourth.map((teamFourth, index) => (
+          <div key={index} onClick={() => openModal(teamFourth)}>
+            <Team_card
+              image={teamFourth.image}
+              name={teamFourth.name}
+              designation={teamFourth.designation}
+              designation_img={teamFourth.designation_img}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="Eee__sub-heading">
+        <p className='Eee__sub-heading-p'>Third Year Student</p>
+      </div>
+
+      <div className="Eee__teams-card-container">
+        {teamThird.map((teamThird, index) => (
+          <div key={index} onClick={() => openModal(teamThird)}>
+            <Team_card
+              image={teamThird.image}
+              name={teamThird.name}
+              designation={teamThird.designation}
+              designation_img={teamThird.designation_img}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="Eee__sub-heading">
+        <p className='Eee__sub-heading-p'>Second Year Student</p>
+      </div>
+
+      <div className="Eee__teams-card-container">
+        {teamSecond.map((teamSecond, index) => (
+          <div key={index} onClick={() => openModal(teamSecond)}>
+            <Team_card
+              image={teamSecond.image}
+              name={teamSecond.name}
+              designation={teamSecond.designation}
+              designation_img={teamSecond.designation_img}
+            />
+          </div>
+        ))}
+      </div>
+
       {isModalOpen && selectedTeamMember && (
         <Modal
           heading={selectedTeamMember.name}
           pic={selectedTeamMember.image}
-          content_1={selectedTeamMember.designation} 
-          content_2="" // You can customize this
-          onClose={closeModal} // Close modal on button click
+          content_1={selectedTeamMember.designation}
+          content_2=""
+          onClose={closeModal}
         />
       )}
     </div>
